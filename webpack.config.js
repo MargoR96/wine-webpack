8ee82d7ad11bae14a71faf9be2057d47882d314d
Shabelnikova Margarita <lovelymargov@gmail.com>
2022-05-05 09:46:18 +0600

    add html-to-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = () => {
     return {
         mode: 'none',
         devtool: false,
+        plugins: [
+            new HtmlWebpackPlugin(
+                {
+                    template: './src/index.html'
+                }
+            )
+        ]
     };
 };
