a27561e282f7527322eeb17853007a45e7df3753
Shabelnikova Margarita <lovelymargov@gmail.com>
2022-05-08 22:31:07 +0600

    add cleanPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// let pathsToClean = ['dist'];
+let cleanOptions = {
+    verbose: true,
+};
 
 module.exports = () => {
     return {
         mode: 'none',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+        },
         devtool: false,
         plugins: [
             new HtmlWebpackPlugin(
                 {
                     template: './src/index.html'
                 }
-            )
+            ),
+            new CleanWebpackPlugin(cleanOptions),
         ]
     };
 };
